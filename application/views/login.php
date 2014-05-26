40bf0df6bd5a0b7ce8b9cb424de939ce083efac7
unscathed18 <unscathed21@hotmail.com>
2014-05-26 17:56:47 +0200

    Ahora tiene login.

--- application/views/login.php
+++ application/views/login.php
@@ -103,34 +103,35 @@
 <body>
 
 <div id="container">
-	<h1>codeigniter-user Library Demo</h1>
+	<h1>Autentificaci&oacute;n de usuario</h1>
 	<div id="body">
-        <p>If you can see this page you have successfully installed the <strong>codeigniter-user</strong> library. <br />There is a simple login form and a private page for you to play around.</p>
-		<p>If you try to access the <a href="<?php echo site_url('login/private_page'); ?>">private page</a> without login you can see the error below.</p>
+        <p>Para poder administrar su restaurante debe primero ingresar sus credenciales.</p>
         <div class="info">
             <p>Login: <strong>admin</strong></p>
             <p>Password: <strong>admin</strong></p>
         </div>
+        <h2 style="color:red;"><?php echo $this->session->flashdata('bad_login'); ?></h2>
+        
         <form action="<?php echo site_url('login/validate') ?>" method="post" id="login_form">
-			<h2>Demo Login Form</h2>
+			<h2>Credenciales</h2>
 			
             <div class="error_message"><?php echo $this->session->flashdata('error_message');?></div>
             <div class="success_message"><?php echo $this->session->flashdata('success_message');?></div>
 			
             <p>
-            <label for="login">User: </label>
+            <label for="login">Usuario: </label>
             <input type="text" name="login" id="login" placeholder="Login" />
             </p>
 
             <p>
-            <label for="password">Password:</label>
+            <label for="password">Contrase&ntilde;a:</label>
             <input type="password" name="password" id="password" placeholder="Password" />
             </p>
 
-            <p><button value="send" class="btn">Login</button></p>
+            <p><button value="send" class="btn">Acceder</button></p>
         </form>
 	</div>
-	<p class="footer">Library by <a href="http://waldir.org/">Waldir Bertazzi Junior</a>. Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+	<p class="footer">Librer&iacute;a por <a href="http://creatalia.besaba.com">Creatalia</a>. P&aacute;gina renderizada en <strong>{elapsed_time}</strong> segundos :)</p>
 </div>
 </body>
 </html>
